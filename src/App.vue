9b7ff037933eb9f72666d864e42659fb12b2e14c
yilun <kingpenguins@163.com>
2020-10-24 21:32:38 +0800

    字体文件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    aaaaa
+    <router-view />
   </div>
 </template>
 
-<style>
+<script>
+export default {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = html.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize
+})
+</script>
+<style lang="scss" scoped>
+@import './assets/styles/global';
+#app {
+  font-size: px2rem(80);
+}
 </style>
